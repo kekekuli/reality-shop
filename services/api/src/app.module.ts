4ae131756b1aab9df195d89ea55345a36b44dc81
kekekuli <kekekuliobj@gmail.com>
2026-07-22 16:30:25 +0800

    feat: add basic graphql module

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { PrismaModule } from "./prisma/prisma.module";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { join } from "node:path";
+import { AppResolver } from "./app.resolver";
 
 @Module({
   controllers: [AppController],
-  imports: [PrismaModule],
+  providers: [AppResolver],
+  imports: [
+    PrismaModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+      sortSchema: true,
+      introspection: process.env.NODE_ENV !== "production",
+    }),
+  ],
 })
 export class AppModule {}
